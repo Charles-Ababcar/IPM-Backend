aea79bd6224c7476290186e0864d1e6f356ed6e0
Sadikh14 <sadikhgueye313@gmail.com>
2023-02-27 13:06:49 +0000

    mise à jour

--- src/main/java/com/poste/ProjetIPM/Repository/IPM_Details_FactureRepository.java
+++ src/main/java/com/poste/ProjetIPM/Repository/IPM_Details_FactureRepository.java
@@ -19,6 +19,8 @@ public interface IPM_Details_FactureRepository  extends JpaRepository<IPM_Detail
     List<IPM_Details_Facture> findByIpmFacture(IPM_Facture ipmFacture);
     //situation des créances globales par période par entité
    // List<IPM_Details_Facture> findByDateSaisieBetween(Date d1, Date d2);
+   // @Query(value = "SELECT new IPM_Details_Facture(c.ipm_employe,c.ipmFacture,SUM(c.part_patient),SUM(c.part_ipm),SUM(c.montant_facture)) from IPM_Details_Facture  c where c.ipmFacture.dateSaisie BETWEEN :d1 and :d2 and c.ipm_employe.ipmEntity.idEntity=:identity GROUP BY c.ipm_employe,c.ipmFacture")
+   // Collection<IPM_Details_Facture> getDateByEntity(@Param("d1") Date d1 ,@Param("d2") Date d2,@Param("identity") Long identity);
     @Query(value = "SELECT new IPM_Details_Facture(c.ipm_employe,c.ipmFacture,SUM(c.part_patient),SUM(c.part_ipm),SUM(c.montant_facture)) from IPM_Details_Facture  c where c.ipmFacture.dateSaisie BETWEEN :d1 and :d2 and c.ipm_employe.ipmEntity.idEntity=:identity GROUP BY c.ipm_employe,c.ipmFacture")
     Collection<IPM_Details_Facture> getDateByEntity(@Param("d1") Date d1 ,@Param("d2") Date d2,@Param("identity") Long identity);
     //la situation des charges totales individuelles et par période
